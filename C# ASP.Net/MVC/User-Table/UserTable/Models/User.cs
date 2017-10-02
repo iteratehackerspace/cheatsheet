5eefb7b5801dc3dbc08cac156fc94a8be4b7e869
Mikazakaryan <Mikazakaryan94@gmail.com>
2017-10-02 15:59:30 +0400

    Simple example of C# ASP.Net MVC app

---
+++ C# ASP.Net/MVC/User-Table/UserTable/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UserTable.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int Priority { get; set; }
+
+        [Required(ErrorMessage = "Required Field")]
+        [StringLength(128, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 128")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Required Field")]
+        [Range(1, 99, ErrorMessage = "Age must be positive and less than 100")]
+        public int? Age { get; set; }
+
+        public List<int> UserIds { get; set; }
+    }
+}
